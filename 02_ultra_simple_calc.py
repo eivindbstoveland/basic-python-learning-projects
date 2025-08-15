4c91374c0da70678e8f7dd3ee63891f168580272
eivindbstoveland <brammer.stoveland@gmail.com>
2025-08-15 13:31:16 +0200

    Update and rename basics_practise_job_2.py to 02_ultra_simple_calc.py
    Just changing the file names and function names to make more sense of it.

--- 02_ultra_simple_calc.py
+++ 02_ultra_simple_calc.py
@@ -1,9 +1,10 @@
-def calc():
+def ultra_simple_calc():
     value1 = int(input('Please enter a first number to calculate: '))
     value2 = int(input('Please enter a second number to calculte: '))
     answer = value1 * value2
 
     print(f'The answer calculated is {answer}')
 
-calc()
+ultra_simple_calc()
+
 
